21b75f04066b177c8d5028915f64e3bd21fd7629
JarnaChao09 <yunyang4928@gmail.com>
2022-03-29 04:05:29 -0400

    add runge kutta 4th order for first and second order ODEs

--- src/main/kotlin/org/kotrix/numeric/DiffEqTest.kt
+++ src/main/kotlin/org/kotrix/numeric/DiffEqTest.kt
@@ -1,6 +1,7 @@
 package org.kotrix.numeric
 
 import org.kotrix.numeric.diffeq.euler
+import org.kotrix.numeric.diffeq.rungeKutta4
 
 fun main() {
     val x00 = 0.0
@@ -8,27 +9,61 @@ fun main() {
     val y00 = 0.0
     val y01 = 25.0
     val xn = 3.0
+
+    val eulerEstimation = euler(x00, y00, xn) { x: Double, _: Double ->
+        2 * x
+    }
+    val rungeKuttaEstimation = rungeKutta4(x00, y00, xn) { x: Double, _: Double ->
+        2 * x
+    }
     println(
-        "Approximation of dy/dx = 2 * x, given initial conditions of ($x00, $y00), y($xn) = ${
-            euler(x00, y00, xn) { x: Double, _: Double ->
-                2 * x
-            }
+        "Euler Approximation of dy/dx = 2 * x, given initial conditions of ($x00, $y00), y($xn) = $eulerEstimation, error = ${
+            kotlin.math.abs(
+                eulerEstimation - 9.0
+            )
         }"
     )
 
     println(
-        "Approximation of dy/dx = 2 * x, given initial conditions of ($x01, $y01), y($xn) = ${
-            euler(x01, y01, xn) { x: Double, _: Double ->
-                2 * x
-            }
+        "RK4 Approximation of dy/dx = 2 * x, given initial conditions of ($x00, $y00), y($xn) = $rungeKuttaEstimation, error = ${
+            kotlin.math.abs(
+                rungeKuttaEstimation - 9.0
+            )
         }"
     )
 
+    println("Exact: ${9.0}\n")
+
+//    println(
+//        "Approximation of dy/dx = 2 * x, given initial conditions of ($x01, $y01), y($xn) = ${
+//            euler(x01, y01, xn) { x: Double, _: Double ->
+//                2 * x
+//            }
+//        }"
+//    )
+//
+    val euler2Estimation = euler(0.0, 1.0, 0.0, 1.0) { _: Double, y: Double, yPrime: Double ->
+        -2 * yPrime - 2 * y
+    }
+
+    val rungeKutta2Estimation = rungeKutta4(0.0, 1.0, 0.0, 1.0) { _: Double, y: Double, yPrime: Double ->
+        -2 * yPrime - 2 * y
+    }
+    //          0.5083259859995251390727160162587141299640797003923103676087958856
+    val exact = 0.508325985999525
     println(
-        "Approximation of d(dy/dx)/dx = -2y' - 2y, given initial conditions of y(0) = 1 and y'(0) = 0, y(1) = ${
-            euler(0.0, 1.0, 0.0, 1.0) { _: Double, y: Double, yPrime: Double ->
-                -2 * yPrime - 2 * y
-            }
+        "Euler Approximation of d(dy/dx)/dx = -2y' - 2y, given initial conditions of y(0) = 1 and y'(0) = 0, y(1) = $euler2Estimation, error = ${
+            kotlin.math.abs(
+                euler2Estimation - exact
+            )
         }"
     )
+    println(
+        "RK4 Approximation of d(dy/dx)/dx = -2y' - 2y, given initial conditions of y(0) = 1 and y'(0) = 0, y(1) = $rungeKutta2Estimation, error = ${
+            kotlin.math.abs(
+                rungeKutta2Estimation - exact
+            )
+        }"
+    )
+    println("Exact: $exact")
 }
---
+++ src/main/kotlin/org/kotrix/numeric/diffeq/RK4.kt
@@ -0,0 +1,88 @@
+package org.kotrix.numeric.diffeq
+
+import org.kotrix.symbolic.funAST.extensions.sec
+
+private fun RK4(
+    initialStart: Double,
+    initialValue: Double,
+    steps: Int,
+    delta: Double,
+    firstDerivative: (Double, Double) -> Double,
+): Double {
+    var x = initialStart
+    var y = initialValue
+
+    for(i in 0 until steps) {
+        val k1 = delta * firstDerivative(x, y)
+        val k2 = delta * firstDerivative(x + 0.5 * delta, y + 0.5 * k1)
+        val k3 = delta * firstDerivative(x + 0.5 * delta, y + 0.5 * k2)
+        val k4 = delta * firstDerivative(x + delta, y + k3)
+        x += delta
+        y += (k1 + 2 * k2 + 2 * k3 + k4) / 6.0
+    }
+
+    return y
+}
+
+private fun RK4SecondOrder(
+    initialStart: Double,
+    initialValue: Double,
+    initialDerivative: Double,
+    steps: Int,
+    delta: Double,
+    secondDerivative: (Double, Double, Double) -> Double,
+): Double {
+    var x = initialStart
+    var y = initialValue
+    var yPrime = initialDerivative
+
+    for(i in 0 until steps) {
+        val j1 = delta * secondDerivative(x, y, yPrime)
+        val k1 = delta * yPrime
+        val j2 = delta * secondDerivative(x + 0.5 * delta, y + 0.5 * k1, yPrime + 0.5 * j1)
+        val k2 = delta * (yPrime + 0.5 * j1)
+        val j3 = delta * secondDerivative(x + 0.5 * delta, y + 0.5 * k2, yPrime + 0.5 * j2)
+        val k3 = delta * (yPrime + 0.5 * j2)
+        val j4 = delta * secondDerivative(x + delta, y + k3, yPrime + j3)
+        val k4 = delta * (yPrime + j3)
+        x += delta
+        yPrime += (j1 + 2 * j2 + 2 * j3 + j4) / 6.0
+        y += (k1 + 2 * k2 + 2 * k3 + k4) / 6.0
+    }
+
+    return y
+}
+
+fun rungeKutta4(
+    initialStart: Double,
+    initialValue: Double,
+    toFind: Double,
+    delta: Double = 1e-5,
+    firstDerivative: (Double, Double) -> Double,
+): Double {
+    return RK4(
+        initialStart,
+        initialValue,
+        kotlin.math.ceil(kotlin.math.abs(toFind - initialStart) / delta).toInt(),
+        if(initialStart > toFind) -delta else delta,
+        firstDerivative
+    )
+}
+
+fun rungeKutta4(
+    initialStart: Double,
+    initialValue: Double,
+    initialDerivative: Double,
+    toFind: Double,
+    delta: Double = 1e-5,
+    secondDerivative: (Double, Double, Double) -> Double,
+): Double {
+    return RK4SecondOrder(
+        initialStart,
+        initialValue,
+        initialDerivative,
+        kotlin.math.ceil(kotlin.math.abs(toFind - initialStart) / delta).toInt(),
+        if(initialStart > toFind) -delta else delta,
+        secondDerivative,
+    )
+}
