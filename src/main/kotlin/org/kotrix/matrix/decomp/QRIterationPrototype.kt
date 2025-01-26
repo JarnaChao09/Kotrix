bfc87898ce57b40c8fcf24e304f2fa4a421442e2
JarnaChao09 <yunyang4928@gmail.com>
2025-01-25 20:53:17 -0500

    Update prototype with initial 2x2 matrix eigenvalue mean-product (trace-determinant) formula TODO: - Implement Householder reflections - Implement Given rotations - Rethink the modular indexing logic (made bugs harder to catch)   - modular indexing was to allow for negative indexes, maybe a check to see if `abs(index) in indexable_range`     - this has been implemented in the vector rewrite, has not propogated to the matrix rewrite - Update QR iteration to utilize 2x2 matrix eigenvalue method - Complete matrix rewrite

--- src/main/kotlin/org/kotrix/matrix/decomp/QRIterationPrototype.kt
+++ src/main/kotlin/org/kotrix/matrix/decomp/QRIterationPrototype.kt
@@ -1,10 +1,45 @@
 package org.kotrix.matrix.decomp
 
+import org.kotrix.complex.Complex
+import org.kotrix.complex.toComplex
 import org.kotrix.matrix.DoubleMatrix
 import org.kotrix.matrix.matMult
 import org.kotrix.utils.by
 import org.kotrix.vector.DoubleVector
 import kotlin.math.absoluteValue
+import kotlin.math.sqrt
+
+fun eigenvalues2x2(matrix: DoubleMatrix): Pair<Complex, Complex> {
+    require(matrix.shape == 2 by 2) {
+        "eigenvalues2x2 only supports 2 x 2 matrices"
+    }
+
+    // val m = matrix.trace() / 2.0
+    // val p = matrix.determinant()
+    //
+    // val sqr = m * m - p
+    //
+    // return if (sqr < 0.0) {
+    //     val sqrt = sqrt(-sqr)
+    //
+    //     val l1 = Complex(m, +sqrt)
+    //     val l2 = Complex(m, -sqrt)
+    //     l1 to l2
+    // } else {
+    //     val sqrt = sqrt(sqr)
+    //
+    //     val l1 = Complex(m + sqrt)
+    //     val l2 = Complex(m - sqrt)
+    //     l1 to l2
+    // }
+
+    val m = (matrix.trace() / 2.0).toComplex()
+    val p = matrix.determinant().toComplex()
+
+    val sqrt = (m * m - p).sqrt
+
+    return (m + sqrt) to (m - sqrt)
+}
 
 fun algorithm(matrix: DoubleMatrix, tolerance: Double = 1e-12, maxIteration: Int = 1000): DoubleVector {
     require(matrix.shape.x == matrix.shape.y) {
@@ -30,11 +65,15 @@ fun algorithm(matrix: DoubleMatrix, tolerance: Double = 1e-12, maxIteration: Int
         i++
     }
 
-    println(curr)
+    // println(curr)
 
     return DoubleVector(matrix.shape.x) {
         curr[it, it]
     } as DoubleVector
+}
+
+fun test(matrix: DoubleMatrix) {
+    println("QR iteration:\n${algorithm(matrix)}\n\nMean Product formula:\n${eigenvalues2x2(matrix)}")
 }
 
 fun main() {
@@ -43,19 +82,24 @@ fun main() {
         2.0, -1.0
     )
 
-    val eigenvalues1 = algorithm(matrix1)
-    println(eigenvalues1)
-
-    // todo: figure out how to use QR iteration to solve for complex eigenvalues
-    val matrix2 = DoubleMatrix.of(6 by 6,
-         7,  3,  4,  11, -9, -2,
-        -6,  4, -5,   7,  1, 12,
-        -1, -9,  2,   2,  9,  1,
-        -8,  0, -1,   5,  0,  8,
-        -4,  3, -5,   7,  2, 10,
-         6,  1,  4, -11, -7, -1,
+    val matrix2 = DoubleMatrix.of(2 by 2,
+        3.0, 1.0,
+        4.0, 1.0,
     )
 
-    val eigenvalues2 = algorithm(matrix2)
-    println(eigenvalues2)
+    test(matrix1)
+    test(matrix2)
+
+    // todo: figure out how to use QR iteration to solve for complex eigenvalues
+    // val matrix2 = DoubleMatrix.of(6 by 6,
+    //      7,  3,  4,  11, -9, -2,
+    //     -6,  4, -5,   7,  1, 12,
+    //     -1, -9,  2,   2,  9,  1,
+    //     -8,  0, -1,   5,  0,  8,
+    //     -4,  3, -5,   7,  2, 10,
+    //      6,  1,  4, -11, -7, -1,
+    // )
+    //
+    // val eigenvalues2 = algorithm(matrix2)
+    // println(eigenvalues2)
 }
